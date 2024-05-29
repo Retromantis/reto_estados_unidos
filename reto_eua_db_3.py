95293b86fc4f24052ea2555bbac5aecdc1428dc6
Victor Zegarra <proveedor141@culqi.com>
2024-05-29 11:36:54 -0500

    ejercicio 2

--- reto_eua_db_3.py
+++ reto_eua_db_3.py
@@ -9,16 +9,20 @@ import mysql.connector as myconector;
 conect= myconector.connect(user='root', password ='', host = 'localhost', database = 'estados_unidos', port = "3306")
 query = conect.cursor()
 
-data_estados = """
+sentence = """
+
 INSERT INTO estados (Id_Estado, Nombre, Fecha_Fundacion, Latitud, Longitud) VALUES 
+   
   (1,"Alabama","14-12-1819","33.258.882", "-86.829.534"),
   (2,"Florida"," 03-03-1845 ","27.756.767 ", "-81.463.983"),
   (3,"Georgia"," 12-02-1733","32.329.381 ", "-83.113.737"),
   (4,"South Carolina"," 26-03-1776","33.687.439", "-80.436.374");
-"""
-query.execute(data_estados)
 
-data_muertes = """
+
+"""
+
+sentence = """
+
 INSERT INTO muertes (Id_Muertes, Id_Estado, Anio, Cantidad) VALUES
     (1, 1, 2000, 10622),
 	(2, 1, 2001, 15912),
@@ -29,9 +33,8 @@ INSERT INTO muertes (Id_Muertes, Id_Estado, Anio, Cantidad) VALUES
     (7, 4, 2000, 8581),
     (8, 4, 2001, 9500);
 """
-query.execute(data_muertes)
 
-data_poblacion = """
+sentence = """
 INSERT INTO poblacion (Id_Poblacion, Id_Estado, Anio, Cantidad) VALUES
     (1, 1, 2000, 4447100),
 	(2, 1, 2001, 4451493),
@@ -41,19 +44,19 @@ INSERT INTO poblacion (Id_Poblacion, Id_Estado, Anio, Cantidad) VALUES
     (6, 3, 2001, 8229823),
     (7, 4, 2000, 4012012),
     (8, 4, 2001, 4023438);
+
 """
-query.execute(data_poblacion)
+sentence = """ INSERT INTO residentes_65 (Id_Residentes, Id_Estado, Anio, Cantidad) VALUES
+        (1, 1, 2000, 3870598),
+		(2, 2, 2001, 3880476),
+		(3, 2, 2000, 13237167),
+		(4, 2, 2001, 13548077),
+		(5, 3, 2000, 7440877),
+		(6, 3, 2001, 7582146),
+		(7, 4, 2000, 3535770),
+		(8, 4, 2001, 3567172);
+"""
+
+query.execute(sentence)
 
 
-data_residentes = """
-INSERT INTO residentes_65 (Id_Residentes, Id_Estado, Anio, Cantidad) VALUES
-    (1, 1, 2000, 3870598),
-    (2, 2, 2001, 3880476),
-    (3, 2, 2000, 13237167),
-    (4, 2, 2001, 13548077),
-    (5, 3, 2000, 7440877),
-    (6, 3, 2001, 7582146),
-    (7, 4, 2000, 3535770),
-    (8, 4, 2001, 3567172);
-"""
-query.execute(data_residentes)
